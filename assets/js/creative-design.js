69db061edc5e8ae866e6b4f9581caf2099a14107
Keiller Nogueira <keillernogueira@gmail.com>
2023-06-04 10:30:02 +0100

    Initial commit

---
+++ assets/js/creative-design.js
@@ -0,0 +1,42 @@
+/*!
+=========================================================
+* Creative Design Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/ 
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+}); 
+
+
+window.onload = function(e){ 
+    console.log("window.onload");
+	$("#menu > a").each(function(){
+		console.log('xxx');
+		$(this).removeClass("active");
+	});
+	$(this).addClass('active');
+}
